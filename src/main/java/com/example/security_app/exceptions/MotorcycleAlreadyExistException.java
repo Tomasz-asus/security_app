31c3d17c652a1b29fab1a86cdc1a79e17616c383
Tomasz-asus <81529362+Tomasz-asus@users.noreply.github.com>
2023-05-18 18:38:37 +0200

    delombok part 2

--- src/main/java/com/example/security_app/exceptions/MotorcycleAlreadyExistException.java
+++ src/main/java/com/example/security_app/exceptions/MotorcycleAlreadyExistException.java
@@ -1,9 +1,5 @@
 package com.example.security_app.exceptions;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
 
 public class MotorcycleAlreadyExistException extends RuntimeException {
 
--- src/main/java/com/example/security_app/exceptions/UserAlreadyExistException.java
+++ src/main/java/com/example/security_app/exceptions/UserAlreadyExistException.java
@@ -1,10 +1,5 @@
 package com.example.security_app.exceptions;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-@Data
-@EqualsAndHashCode(callSuper = true)
 public class UserAlreadyExistException extends RuntimeException {
 
     public UserAlreadyExistException(String message) {
@@ -15,21 +10,5 @@ public class UserAlreadyExistException extends RuntimeException {
         return "UserAlreadyExistException()";
     }
 
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof UserAlreadyExistException)) return false;
-        final UserAlreadyExistException other = (UserAlreadyExistException) o;
-        if (!other.canEqual((Object) this)) return false;
-        if (!super.equals(o)) return false;
-        return true;
     }
 
-    protected boolean canEqual(final Object other) {
-        return other instanceof UserAlreadyExistException;
-    }
-
-    public int hashCode() {
-        int result = super.hashCode();
-        return result;
-    }
-}
